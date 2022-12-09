48c8ed1fa5355539c6df0427125d3089fb4e3529
Jackfinal <Jackfinal@users.noreply.github.com>
2022-12-09 10:04:28 +0800

    Update Dm8ValidationServiceProvider.php

--- src/Dm8/Dm8ValidationServiceProvider.php
+++ src/Dm8/Dm8ValidationServiceProvider.php
@@ -3,7 +3,7 @@
 namespace LaravelDm8\Dm8;
 
 use Illuminate\Validation\ValidationServiceProvider;
-use LaravelDm8\Oci8\Validation\Dm8DatabasePresenceVerifier;
+use LaravelDm8\Dm8\Validation\Dm8DatabasePresenceVerifier;
 
 class Dm8ValidationServiceProvider extends ValidationServiceProvider
 {
