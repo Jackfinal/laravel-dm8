85c5c2fc7cfc809cd948a0b3062565c6fa415255
Jackfinal <Jackfinal@users.noreply.github.com>
2023-08-08 11:13:13 +0800

    Update DmConnector.php
    更新大小写字段错误问题

--- src/Dm8/Connectors/DmConnector.php
+++ src/Dm8/Connectors/DmConnector.php
@@ -15,7 +15,7 @@ class DmConnector extends Connector implements ConnectorInterface
      * @var array
      */
     protected $options = [
-        PDO::CASE_LOWER               => PDO::CASE_LOWER,
+        PDO::ATTR_CASE               => PDO::CASE_LOWER,
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_ORACLE_NULLS       => PDO::NULL_NATURAL,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
